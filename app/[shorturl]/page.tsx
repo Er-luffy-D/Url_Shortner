882fd725f3512dd306f166b593fe32450234c7eb
Piyush Dixit <70336252+Er-luffy-D@users.noreply.github.com>
2025-04-13 13:47:24 +0530

    fixing deployment error

--- app/[shorturl]/page.tsx
+++ app/[shorturl]/page.tsx
@@ -2,8 +2,9 @@ import { prisma } from "@/lib/prisma";
 import RedirectClient from "./RedirectClient";
 
 export default async function Page({ params }: { params: { shorturl: string } }) {
+  const { shorturl } = await params;
   const data = await prisma.link.findFirst({
-    where: { shortCode: params.shorturl },
+    where: { shortCode: shorturl },
     select: { url: true },
   });
 
