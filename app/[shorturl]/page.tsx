01bed6aa7f99af9519fb1e9a5e1463302f0fc03a
Piyush Dixit <70336252+Er-luffy-D@users.noreply.github.com>
2025-04-13 13:58:52 +0530

    updated page.tsx

--- app/[shorturl]/page.tsx
+++ app/[shorturl]/page.tsx
@@ -2,9 +2,8 @@ import { prisma } from "@/lib/prisma";
 import RedirectClient from "./RedirectClient";
 
 export default async function Page({ params }: { params: { shorturl: string } }) {
-  const { shorturl } = await params;
   const data = await prisma.link.findFirst({
-    where: { shortCode: shorturl },
+    where: { shortCode: params.shorturl },
     select: { url: true },
   });
 
